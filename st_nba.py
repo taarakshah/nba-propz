fc374483a319af8ff83d22f10ed0b3fb91aa1ccb
shahx272 <shahx272@umn.edu>
2023-02-08 16:03:37 -0600

    v2.1.0 session state for df update

--- st_nba.py
+++ st_nba.py
@@ -5,6 +5,7 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 import time
+import git
 
 def fix_dates(df):
     '''
@@ -135,100 +136,113 @@ def get_new_data():
 
 st.set_page_config(page_title='PROPZ v2.1.0', page_icon=':basketball:', layout="wide", initial_sidebar_state="auto", menu_items=None)
 
-c1, c2 = st.columns(2)
-with c2:
-    ## Button for getting new data from FantasyPros
-    newdata = st.button('Gather new NBA player prop data', help='Gets new NBA player prop data from FantasyPros. Returns data of all players above 25 min/game.')
-    if newdata:
-        start = time.time()
-        gamelog = get_new_data()
-        end = time.time()
-        st.write('New game log took {} seconds to gather.'.format( np.round(end-start,1) ) )
+
+def main():
+    c1, c2 = st.columns(2)
+    with c2:
+        ## Button for getting new data from FantasyPros
+        newdata = st.button('Gather new NBA player prop data', help='Gets new NBA player prop data from FantasyPros. Returns data of all players above 25 min/game.')
+        if newdata:
+            start = time.time()
+            gamelog = get_new_data()
+            end = time.time()
+            st.write('New game log took {} seconds to gather.'.format( np.round(end-start,1) ) )
+        else:
+            ## read in default gamelog
+            gamelog = pd.read_csv('gamelog.csv')
+            gamelog = fix_dates(gamelog)
+    with c1:
+        if newdata:
+            st.write('Game log is current as of today.')
+        else:
+            st.write('Game log is current as of Feb 6, 2023.')
+            st.write('Click the button to refresh. Will take approx ~90 seconds.')
+
+    S = st.session_state()
+    S.gamelog = gamelog
+
+
+    st.markdown("<h1 style='text-align: center;'>NBA PROPZ 2.1.0</h1>", unsafe_allow_html=True)
+    hide_menu_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            </style>
+            """
+    st.markdown(hide_menu_style, unsafe_allow_html=True)
+
+    def convert_df(df):
+    return df.to_csv(index=False).encode('utf-8')
+
+
+    ## SIDEBAR / OPTIONS MENU
+    st.sidebar.markdown("## Options Menu")
+
+    ## get unique teams
+    teams_list = S.gamelog['TEAM'].unique().tolist()
+    ## team option
+    team_subset = st.sidebar.selectbox('Team', options=['Choose Team'] + teams_list)
+
+    ## if a team is chosen, only show players from that team
+    if team_subset=='Choose Team':
+        player_list = S.gamelog['NAME'].unique().tolist()
     else:
-        ## read in default gamelog
-        gamelog = pd.read_csv('gamelog.csv')
-        gamelog = fix_dates(gamelog)
-with c1:
-    if newdata:
-        st.write('Game log is current as of today.')
-    else:
-        st.write('Game log is current as of Feb 6, 2023.')
-        st.write('Check the box to refresh. Will take approx ~90 seconds.')
+        player_list = S.gamelog[S.gamelog['TEAM']==team_subset]['NAME'].unique().tolist()
+    ## player option
+    player_subset = st.sidebar.selectbox('Player', options=['Choose Player'] + player_list)
+
+    ## prop sliders
+    thres_pts = st.sidebar.number_input('Points', value=19.5, step=1.0, help='Adjust threshold for points to compare prop trends.')
+    thres_reb = st.sidebar.number_input('Rebounds', value=5.5, step=1.0, help='Adjust threshold for rebounds to compare prop trends.')
+    thres_ast = st.sidebar.number_input('Assists', value=3.5, step=1.0, help='Adjust threshold for assists to compare prop trends.')
+    thres_3pm = st.sidebar.number_input('Threes', value=1.5, step=1.0, help='Adjust threshold for threes to compare prop trends.')
+    thres_pra = st.sidebar.number_input('Pts/Reb/Ast', value=24.5, step=1.0, help='Adjust threshold for PRA to compare prop trends.')
 
 
-st.markdown("<h1 style='text-align: center;'>NBA PROPZ 2.1.0</h1>", unsafe_allow_html=True)
-hide_menu_style = """
-        <style>
-        #MainMenu {visibility: hidden;}
-        </style>
-        """
-st.markdown(hide_menu_style, unsafe_allow_html=True)
-
-def convert_df(df):
-   return df.to_csv(index=False).encode('utf-8')
+    st.header('Show Plots Over Time')
+    c1,c2,c3,c4,c5 = st.columns(5)
+    with c1: plot_pts = st.button('Points')
+    with c2: plot_reb = st.button('Rebounds')
+    with c3: plot_ast = st.button('Assists')
+    with c4: plot_3pm = st.button('Threes')
+    with c5: plot_pra = st.button('Pts/Reb/Ast')
 
 
-## SIDEBAR / OPTIONS MENU
-st.sidebar.markdown("## Options Menu")
+    ## Print trends
+    if player_subset!='Choose Player':
+        st.header('{} has hit...'.format(player_subset))
+        l5 = S.gamelog[S.gamelog['NAME']==player_subset].head(5)
+        l10 = S.gamelog[S.gamelog['NAME']==player_subset].head(10)
+        l25 = S.gamelog[S.gamelog['NAME']==player_subset].head(25)
 
-## get unique teams
-teams_list = gamelog['TEAM'].unique().tolist()
-## team option
-team_subset = st.sidebar.selectbox('Team', options=['Choose Team'] + teams_list)
+        st.write('...over {} points in {}/5, {}/10, and {}/25'.format(thres_pts, sum(l5['PTS'] >= thres_pts), sum(l10['PTS'] >= thres_pts), sum(l25['PTS'] >= thres_pts) ) )
+        st.write('...over {} rebounds in {}/5, {}/10, and {}/25'.format(thres_reb, sum(l5['REB'] >= thres_reb), sum(l10['REB'] >= thres_reb), sum(l25['REB'] >= thres_reb) ) )
+        st.write('...over {} assists in {}/5, {}/10, and {}/25'.format(thres_ast, sum(l5['AST'] >= thres_ast), sum(l10['AST'] >= thres_ast), sum(l25['AST'] >= thres_ast) ) )
+        st.write('...over {} threes in {}/5, {}/10, and {}/25'.format(thres_3pm, sum(l5['3PM'] >= thres_3pm), sum(l10['3PM'] >= thres_3pm), sum(l25['3PM'] >= thres_3pm) ) )
+        st.write('...over {} PRA in {}/5, {}/10, and {}/25'.format(thres_pra, sum(l5['PRA'] >= thres_pra), sum(l10['PRA'] >= thres_pra), sum(l25['PRA'] >= thres_pra) ) )
 
-## if a team is chosen, only show players from that team
-if team_subset=='Choose Team':
-    player_list = gamelog['NAME'].unique().tolist()
-else:
-    player_list = gamelog[gamelog['TEAM']==team_subset]['NAME'].unique().tolist()
-## player option
-player_subset = st.sidebar.selectbox('Player', options=['Choose Player'] + player_list)
+    ## Associated detailed data / descriptions
+    st.header('Full Game Log for Chosen Player')
+    st.write('If a player is chosen with the filter menu on the left, it will show the game log of that player. Otherwise, it will show the full game log for all possible players and teams.')
 
-## prop sliders
-thres_pts = st.sidebar.number_input('Points', value=19.5, step=1.0, help='Adjust threshold for points to compare prop trends.')
-thres_reb = st.sidebar.number_input('Rebounds', value=5.5, step=1.0, help='Adjust threshold for rebounds to compare prop trends.')
-thres_ast = st.sidebar.number_input('Assists', value=3.5, step=1.0, help='Adjust threshold for assists to compare prop trends.')
-thres_3pm = st.sidebar.number_input('Threes', value=1.5, step=1.0, help='Adjust threshold for threes to compare prop trends.')
-thres_pra = st.sidebar.number_input('Pts/Reb/Ast', value=24.5, step=1.0, help='Adjust threshold for PRA to compare prop trends.')
+    ## Consider all cases for displaying data
+    ## Do not choose team or player -> show full log
+    if (team_subset=='Choose Team') & (player_subset=='Choose Player'):
+        st.dataframe(S.gamelog)
+    ## Choose team but not a player -> show log for all players on chosen team
+    elif (team_subset!='Choose Team') & (player_subset=='Choose Player'):
+        st.dataframe(S.gamelog[S.gamelog.TEAM==team_subset].reset_index(drop=True))
+    ## Choose player -> show log for chosen player
+    elif player_subset!='Choose Player':
+        st.dataframe(S.gamelog[S.gamelog.NAME==player_subset].reset_index(drop=True))
 
+    ## Button for downloading gamelog data to CSV
+    gamelog_csv = convert_df(S.gamelog)
+    st.download_button("Download CSV of full game log for all players", gamelog_csv, "gamelog.csv", "text/csv", key='download-csv')
 
-st.header('Show Plots Over Time')
-c1,c2,c3,c4,c5 = st.columns(5)
-with c1: plot_pts = st.button('Points')
-with c2: plot_reb = st.button('Rebounds')
-with c3: plot_ast = st.button('Assists')
-with c4: plot_3pm = st.button('Threes')
-with c5: plot_pra = st.button('Pts/Reb/Ast')
-
-
-## Print trends
-if player_subset!='Choose Player':
-    st.header('{} has hit...'.format(player_subset))
-    l5 = gamelog[gamelog['NAME']==player_subset].head(5)
-    l10 = gamelog[gamelog['NAME']==player_subset].head(10)
-    l25 = gamelog[gamelog['NAME']==player_subset].head(25)
-
-    st.write('...over {} points in {}/5, {}/10, and {}/25'.format(thres_pts, sum(l5['PTS'] >= thres_pts), sum(l10['PTS'] >= thres_pts), sum(l25['PTS'] >= thres_pts) ) )
-    st.write('...over {} rebounds in {}/5, {}/10, and {}/25'.format(thres_reb, sum(l5['REB'] >= thres_reb), sum(l10['REB'] >= thres_reb), sum(l25['REB'] >= thres_reb) ) )
-    st.write('...over {} assists in {}/5, {}/10, and {}/25'.format(thres_ast, sum(l5['AST'] >= thres_ast), sum(l10['AST'] >= thres_ast), sum(l25['AST'] >= thres_ast) ) )
-    st.write('...over {} threes in {}/5, {}/10, and {}/25'.format(thres_3pm, sum(l5['3PM'] >= thres_3pm), sum(l10['3PM'] >= thres_3pm), sum(l25['3PM'] >= thres_3pm) ) )
-    st.write('...over {} PRA in {}/5, {}/10, and {}/25'.format(thres_pra, sum(l5['PRA'] >= thres_pra), sum(l10['PRA'] >= thres_pra), sum(l25['PRA'] >= thres_pra) ) )
-
-## Associated detailed data / descriptions
-st.header('Full Game Log for Chosen Player')
-st.write('If a player is chosen with the filter menu on the left, it will show the game log of that player. Otherwise, it will show the full game log for all possible players and teams.')
-
-## Consider all cases for displaying data
-## Do not choose team or player -> show full log
-if (team_subset=='Choose Team') & (player_subset=='Choose Player'):
-    st.dataframe(gamelog)
-## Choose team but not a player -> show log for all players on chosen team
-elif (team_subset!='Choose Team') & (player_subset=='Choose Player'):
-    st.dataframe(gamelog[gamelog.TEAM==team_subset].reset_index(drop=True))
-## Choose player -> show log for chosen player
-elif player_subset!='Choose Player':
-    st.dataframe(gamelog[gamelog.NAME==player_subset].reset_index(drop=True))
-
-## Button for downloading gamelog data to CSV
-gamelog_csv = convert_df(gamelog)
-st.download_button("Download CSV of full game log for all players", gamelog_csv, "gamelog.csv", "text/csv", key='download-csv')
+if __name__=='__main__':
+    main()
+    for key in st.session_state:
+        try:
+            st.session_state[key] = st.session_state[key]
+        except:
+            pass
