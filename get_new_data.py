adc8983a0670a60e6e72f1cd93d25180d8d36b89
shahx272 <shahx272@umn.edu>
2023-02-09 14:13:21 -0600

    mpg >= 15

--- get_new_data.py
+++ get_new_data.py
@@ -39,7 +39,7 @@ def get_new_data():
     ## make it a dataframe
     players = pd.DataFrame(list(zip(names, teams, hrefs, mpgs)), columns=['name','team', 'href','mpg'])
 
-    players25 = players[players['mpg'] >= 25.0].sort_values(by='team').reset_index(drop=True)
+    players25 = players[players['mpg'] >= 15.0].sort_values(by='team').reset_index(drop=True)
     ## split the .php ending for each player to get game log links..need it like trae-young.php, luka-doncic.php, etc
     phplist = []
     for i in range(len(players25.href)):
@@ -101,8 +101,8 @@ def get_new_data():
             except: pass
 
             ## make it a dataframe
-            minilog = pd.DataFrame(list(zip(dates, opps, mins, fgms, fgas, tpms, tpas, ftms, ftas, rebs, asts, tos, stls, blks, pfs, pts, pras)),
-                                columns=['DATE','OPP', 'MIN','FGM','FGA','3PM','3PA','FTM','FTA','REB','AST','TO','STL','BLK','PF','PTS','PRA'])
+            minilog = pd.DataFrame(list(zip(dates, opps, mins, fgms, fgas, tpms, tpas, ftms, ftas, rebs, asts, tos, stls, blks, pfs, pts, pras)), columns=['DATE','OPP','MIN','FGM','FGA','3PM','3PA','FTM','FTA','REB','AST','TO','STL','BLK','PF','PTS','PRA'])
+
         player_name = players['name'][i]
         player_team = players['team'][i]
         minilog['NAME'] = player_name
